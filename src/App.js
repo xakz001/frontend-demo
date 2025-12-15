7d82e250d32bbfcfefaa9389690f08e31a03b0da
Khandamir <dameer.db@gmail.com>
2025-12-15 19:03:04 +0500

    feat: frontend call /api/hello

--- src/App.js
+++ src/App.js
@@ -2,31 +2,24 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("/api/time")
-      .then((res) => {
-        if (!res.ok) throw new Error("Backend not reachables");
-        return res.json();
-      })
-      .then((json) => setData(json))
-      .catch((err) => setError(err.message));
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((json) => setData(json));
   }, []);
 
   return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Frontend → Backend Test</h1>
-
-      {error && <p style={{ color: "red" }}>{error}</p>}
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Frontend mini check</h1>
 
       {data ? (
-        <div>
+        <>
+          <p><b>Service:</b> {data.service}</p>
           <p><b>Message:</b> {data.message}</p>
-          <p><b>Time:</b> {data.time}</p>
-        </div>
+        </>
       ) : (
-        !error && <p>Loading...</p>
+        <p>Loading from backend...</p>
       )}
     </div>
   );
